44d73259e99336562d9ce739738beffa46a36577
Revolt <revolt@localhost.localdomain>
2021-11-15 23:40:42 +0100

    initial commit

---
+++ src/main/java/com/HatchwaysBlogApp/HatchwaysBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.HatchwaysBlogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatchwaysBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatchwaysBlogAppApplication.class, args);
+	}
+
+}
